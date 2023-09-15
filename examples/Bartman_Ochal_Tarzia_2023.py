c68fa91e0a65b01308220b0569f206d3b8841336
Piotr <prbartman@gmail.com>
2023-09-15 05:19:30 +0200

    WIP

--- examples/Bartman_Ochal_Tarzia_2023.py
+++ examples/Bartman_Ochal_Tarzia_2023.py
@@ -116,7 +116,7 @@ def main(config: Config):
     """
     alphas = [1e-2, 1, 1e1, 1e3, 1e4, np.inf]
     ihs = [4, 8, 16, 32, 64, 128, 256]
-    alphas = alphas[:-1]
+    alphas = alphas[-2:-1]
     ihs = ihs[:5]  # TODO
 
     for ih in ihs:
